abb1d1105acb114263e5aa6c25e15870f7e91c18
Arjun Kumar <arjunkumar01@gmail.com>
2020-02-29 20:00:57 -0500

    Commit

--- core/models.py
+++ core/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class item(models.Model):
+class Item(models.Model):
     name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
